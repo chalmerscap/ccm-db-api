288e6143ccb699ce70a9539b3bf52ac5ad302f60
Max Johansson <contact@maxjohansson.se>
2020-07-23 17:10:21 +0200

    Changed package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="borsdata-api-utils-maxijohansson", # Replace with your own username
+    name="borsdata_api_utils_maxijohansson", # Replace with your own username
     version="0.0.1",
     author="Max Johansson",
     description="Borsdata api functions",
