b25ff864ac9f225d9deacb987606a8039eb264dc
Max Johansson <contact@maxjohansson.se>
2020-07-29 00:46:42 +0200

    Name change

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="borsdata-api-utils-maxijohansson", # Replace with your own username
+    name="ccm-db-api-maxijohansson", # Replace with your own username
     version="0.0.1",
     author="Max Johansson",
-    description="Borsdata api functions",
+    description="CCM database api",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/maxijohansson/borsdata_api_utils",
+    url="https://github.com/maxijohansson/ccm_db_api",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
