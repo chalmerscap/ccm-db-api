f95de712c62b399064b281907af17bbfc997c5c2
Max Johansson <contact@maxjohansson.se>
2020-08-04 17:33:06 +0200

    Added print_json to all functions

--- ccm_db_api/borsdata_db.py
+++ ccm_db_api/borsdata_db.py
@@ -11,64 +11,85 @@ class Borsdata_db:
         self.params = {'authKey': key, 'maxYearCount': '20', 'maxcount': '20'}
         self.url_base = 'https://apiservice.borsdata.se/v1/'
 
+
     # Returns dataframe of all available instruments in Börsdata API
-    def instruments(self):
+    def instruments(self, print=False):
         endpoint = 'instruments'
         content = requests.get(self.url_base + endpoint, self.params).json()
+        if print:
+            self.print_json(content)
 
         return json_normalize(content['instruments'])
 
+
     # Returns dataframe of yearly history for a certain ticker and kpi
-    def kpi_history(self, ticker_id, kpi_id):
+    def kpi_history(self, ticker_id, kpi_id, print=False):
         endpoint = 'instruments/{}/kpis/{}/{}/{}/history'.format(str(ticker_id), str(kpi_id) , 'year', 'mean')
         content = requests.get(self.url_base + endpoint, self.params).json()
+        if print:
+            self.print_json(content)
 
         return json_normalize(content['values'])
+
 
     # Returns dataframe of a certain kpi for all tickers
-    def kpi_all(self, kpi_id):
+    def kpi_all(self, kpi_id, print=False):
         endpoint = 'instruments/kpis/{}/{}/{}'.format(str(kpi_id), '1year', 'mean')
         content = requests.get(self.url_base + endpoint, self.params).json()
+        if print:
+            self.print_json(content)
 
         return json_normalize(content['values'])
 
+
     # Returns dataframe of historical daily stock prices for a certain ticker
-    def prices(self, ticker_id):
+    def prices(self, ticker_id, print=False):
         endpoint = 'instruments/{}/stockprices'.format(str(ticker_id))
         content = requests.get(self.url_base + endpoint, self.params).json()
+        if print:
+            self.print_json(content)
         
         return json_normalize(content['stockPricesList'])
 
+
     # Returns dataframe of all reports for a certain ticker
-    def reports(self, ticker_id):
+    def reports(self, ticker_id, print=False):
         endpoint = 'instruments/{}/reports'.format(str(ticker_id))
         content = requests.get(self.url_base + endpoint, self.params).json()
+        if print:
+            self.print_json(content)
 
         return json_normalize(content['reportsYear'])
 
 
-    def report_meta(self):
+    def report_meta(self, print=False):
         endpoint = 'instruments/reports/metadata'
         content = requests.get(self.url_base + endpoint, self.params).json()
+        if print:
+            self.print_json(content)
 
         return json_normalize(content['reportMetadatas'])
 
 
-    def prices_last(self):
+    def prices_last(self, print=False):
         endpoint = 'instruments/stockprices/last'
         content = requests.get(self.url_base + endpoint, self.params).json()
+        if print:
+            self.print_json(content)
 
         return json_normalize(content['stockPricesList'])
 
 
-    def prices_date(self, date):
+    def prices_date(self, date, print=False):
         endpoint = 'instruments/stockprices/date'
         self.params['date'] = date
         content = requests.get(self.url_base + endpoint, self.params).json()
+        if print:
+            self.print_json(content)
 
         return json_normalize(content['stockPricesList'])
 
 
     # Prints the output of requests.get() in a readable manner
-    def print_json(self,    content):
+    def print_json(self, content):
         print(json.dumps(content, indent=4, sort_keys=True))
---
+++ ccm_db_api/ccm_db.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+
+class Ccm_db:
+
+	def __init__(self, password)
+
+		self.cnx, self.cursor = self.open_connection()
+
+
+	def open_connection():
+	    cnx = mysql.connector.connect(
+	    host="185.51.226.6",
+	    user="CCM",
+	    passwd=password,
+	    database="CCM"
+	    )
+	    cursor = cnx.cursor()
+
+	    return cnx, cursor
+
+
+	def close_connection(cnx, cursor, commit):
+	    if commit == 1:
+	        cnx.commit()
+	    cursor.close()
+	    cnx.close()
