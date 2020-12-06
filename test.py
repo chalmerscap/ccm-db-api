e1fc354e5fafd706377b47998eec1d6ae48b8ef3
unknown <contact@maxjohansson.se>
2020-12-06 13:07:46 +0100

    Moved files

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import ccm_db_api as api
 import matplotlib.pyplot as plt
 
-key = ''
+key = 'kLP2Dr4O8s4iaGuYRsKXM7UWlvYQDLN25CHn1rGZ'
 guldgruvan = api.Guldgruvan(key)
 
 # df = guldgruvan.prices_daily('AAK.ST', '2018-01-01', '2018-02-01', print_json=0)
 # df = guldgruvan.report_year('AAK.ST', '2018', print_json=0)
-# df = guldgruvan.instruments(print_json=0)
-print(df.columns)
+df = guldgruvan.instruments(print_json=0)
+print(df)
 
 # plt.plot(df['date'], df['close'])
 # plt.show()
